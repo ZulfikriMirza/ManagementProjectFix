7fd79a8b6827a2af34a67de8817053e6d149006e
Muhammad Rifaldi B <rifaldibadu@gmail.com>
2021-03-08 21:16:55 +0700

    color_edit_aboutus

--- resources/views/AboutUs/aboutus.blade.php
+++ resources/views/AboutUs/aboutus.blade.php
@@ -11,13 +11,9 @@
 <main>
 <div class="container">
         <h1>About Us</h1>
-        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
 </main>
-<br></br>
-    
-<div class="container text-center">
-    <hr></hr>
+
     <div class="container text-center">
         <h2> What We Are About</h2>
         <p>We're here to show you some our masterpiece...Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur dipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Proin gravida nibh
