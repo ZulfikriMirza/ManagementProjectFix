e3d4ba96eb673901a7d5e466815921a9d3f7d79e
mahesaAtma <37969344+mahesaAtma@users.noreply.github.com>
2021-03-12 01:04:26 +0700

    Finalizing Navbar Styling

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -3,4 +3,13 @@ $(document).ready(() => {
         $(`.navbars .navbars-rights > .cart-show`).toggleClass("show-cart");
         console.log(showCart);
     })
-})
+});
+
+$(`.isi-hamburger-menu i`).click(() => {
+    $(`.isi-hamburger-menu .cart-show`).toggleClass("show-cart");
+});
+
+$(`.hamburger-menu`).click(() => {
+    $(`.isi-hamburger-menu`).toggleClass("show-menu");
+    console.log(showCart);
+});
--- resources/views/navbar/index.blade.php
+++ resources/views/navbar/index.blade.php
@@ -14,4 +14,27 @@
         </div>
         <h4><a href="#">LOGIN</a> | <a href="#">REGISTER</a></h4>
     </div>
+    <div class="hamburger-menu">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</div>
+<div class="isi-hamburger-menu">
+    <div class="row">
+        <div class="col-7">
+            <p><a href="{{ route('aboutus') }}"><span></span>About Us<span></span></a></p>
+            <p><a href="{{ route('showcase') }}"><span></span>Showcase<span></span></a></p>
+            <p><a href="{{ route('contact') }}"><span></span>Contact<span></span></a></p>
+            <p><a href="{{ route('checkout') }}"><span></span>Checkout<span></span></a></p>
+            <h4><a href="#">LOGIN</a> | <a href="#">REGISTER</a></h4>
+        </div>
+        <div class="col-5 ham-right">
+            <i class="fas fa-shopping-cart"></i>
+            <div class="cart-show" id="cart-show-id">
+                <p class="mt-2" style="color: #282828;">Yet! Cart is empty!</p>
+                <img src="{{ asset('Home/Materials/3298067.jpg')}}">
+            </div>
+        </div>
+    </div>
 </div>
