20a08f1fdb183e6dbb976ae71054f690e71fa43c
Zulfikri Mirza <zulfikrimirza@yahoo.com>
2021-03-11 19:31:42 +0700

    checkout page sementara

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use App\Http\Controllers\ListJasaController;
 use App\Http\Controllers\Showcase;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserPageController;
 use App\Http\Controllers\AboutUsController;
 use App\Http\Controllers\ContactController;
-use App\Http\Controllers\HomeController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 
@@ -20,23 +21,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-
-//------------------Controller HOME------------------//
-Route::get('/', [HomeController::class, 'index']);
-
-
 
 //------------------Controller List Jasa------------------//
 Route::get('/listjasa', [ListJasaController::class, 'index']);
 
 
 
+
 //------------------Controller Showcase------------------//
 Route::get('/showcase', [Showcase::class, 'index']);
+
+
+
+//------------------Controller Home------------------//
+Route::get('/', [HomeController::class, 'index']);
 
 
 //------------------Controller User------------------//
@@ -52,3 +57,8 @@ Route::get('/aboutus', [AboutUsController::class, 'index']);
 
 //------------------Controller Contact------------------//
 Route::get('/contact', [ContactController::class, 'index']);
+
+
+//------------------Controller Contact------------------//
+Route::get('/checkout', [CartController::class, 'index']);
+
