ae4ae3b97d27cd1f886b25970ea6c7ec61b8eb35
mahesaAtma <37969344+mahesaAtma@users.noreply.github.com>
2021-03-13 12:17:12 +0700

    Finalizing HomePage

--- resources/views/Home/listJasa.blade.php
+++ resources/views/Home/listJasa.blade.php
@@ -1,14 +1,14 @@
 <div class="listjasa">
     <h1>List Jasa</h1>
     <div class="items">
-        <div class="desainFurnitur">
+        <div class="desainFurnitur" data-aos="fade-up" data-aos-delay="300">
             <img src="{{ asset('Home/Materials/material_0002_Desain_Furniture.png') }}">
             <div class="text-listjasa">
                 <h3>Desain Furnitur</h3>
             </div>
             <button type="button" class="btn btn-secondary">Pesan</button>
         </div>
-        <div class="arsitekturBangunan">
+        <div class="arsitekturBangunan" data-aos="fade-up" data-aos-delay="600">
             <img src="{{ asset('Home/Materials/material_0003_Arsitektur-Bangunan.png') }}">
             <div class="text-listjasa">
                 <h3>Arsitektur Bangunan</h3>
@@ -16,7 +16,7 @@
             <button type="button" class="btn btn-secondary">Pesan</button>
 
         </div>
-        <div class="desainInterior">
+        <div class="desainInterior" data-aos="fade-up" data-aos-delay="900">
             <img src="{{ asset('Home/Materials/material_0001_Desain_Produk.png') }}">
             <div class="text-listjasa">
                 <h3>Desain Interior</h3>
