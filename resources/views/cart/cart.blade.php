be68f8abe84da12881b2105a5f0a22e1296c6656
Zulfikri Mirza <zulfikrimirza@yahoo.com>
2021-03-16 11:10:30 +0700

    31%

--- resources/views/cart/cart.blade.php
+++ resources/views/cart/cart.blade.php
@@ -10,34 +10,71 @@
 @include('navbar.index')
 
 <div class="container">
-  <h1 style="text-align: center">Checkout your order here !</h1>
-
-  <div class="row">
-    <div class="card">
-      <div class="col-md-4 order-md-2 mb-4 ">
-        <h4 class="d-flex justify-content-between align-items-center mb-3">
-          <span>Daftar Pesanan</span>
-        </h4>
-        <ul class="list-group mb-3 ">
-          <li class="list-group-item d-flex justify-content-between lh-condensed">
-            <div>
-              <h6 class="my-0"></h6>
-              <small class="text-muted">Jumlah Produk yang dipesan : ------------------------------- </small>
-            </div>
-          </li>
-          <li class="list-group-item d-flex justify-content-between">
-            <span>Harga Total : ---------------------------------------------------------------</span>
-            <strong></strong>
-          </li>
-        </ul>
-        <li class="list-group-item d-flex justify-content-between">
-          <strong>Tidak Ada Item</strong>
-        </li>
-
+<div class="row d-flex justify-content-center">
+    <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3 ">
+      <div class="table-responsive-sm">
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">No</th>
+              <th scope="col">Produk Name</th>
+              <th scope="col">Quantity</th>
+              <th scope="col">Harga(Rp)</th>
+            </tr>
+          </thead>
+          <!-- {{$number=1}} jgn dihapus-->
+            @foreach($jasa as $jasa)
+          <tbody>
+             <tr>
+               <th scope="row">{{$number++}}</th>
+               <td>{{$produk['item']['nama']}}</td>
+               <td><span class="badge badge-pill badge-danger text-center">{{$produk['qty']}} </span></td>
+               <td>{{ number_format($produk['harga'],0,",",".")}}</td>
+               <td>
+                <a href="{{route('Cart.plus',['id'=>$produk['item']['id']])}}"><i class="fas fa-plus-circle"></i></a>
+               </td>
+               <td>
+                 <a href="{{route('Cart.reduceOne',['id'=>$produk['item']['id']])}}">  <i class="fas fa-minus-circle"></i></a>
+               </td>
+               <td>
+                 <a href="{{route('Cart.remove',['id'=>$produk['item']['id']])}}">  <i class="far fa-trash-alt"></i></a>
+               </td>
+             </tr>
+           </tbody>
+           @endforeach
+        </table>
       </div>
+
+      </ul>
     </div>
   </div>
-
+  <div class="row d-flex justify-content-center">
+    <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
+      <strong>Total Harga : Rp. {{number_format($totalHarga,0,",",".")}}</strong>
+    </div>
+  </div>
+  <hr>
+  <div class="row d-flex justify-content-center">
+    <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
+      <a href="{{route('checkout')}}" type="button" class="btn btn-success">Checkout</a>
+    </div>
+  </div>
+  <br>
+  <!-- image -->
+  <div class="image img-fluid d-flex mt-5 justify-content-center">
+    <img src="img/cart_visual.png" alt="cart visual">
+  </div>
+  @else
+  <div class="row d-flex justify-content-center">
+    <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
+      <h2 class="text-center mt-5">Tidak ada item</h2>
+    </div>
+  </div>
+  <div class="image d-flex mt-5 justify-content-center">
+    <img src="foto/cart_empty.png" alt="cart visual">
+  </div>
+</div>
+@endif
 
 
 </div>
