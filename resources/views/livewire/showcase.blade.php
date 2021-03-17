42a8b63f0e64390ea8ef0a36e794febb43269882
Zulfikri Mirza <zulfikrimirza@yahoo.com>
2021-03-17 15:32:09 +0700

    showcase done tinggal js nya

--- resources/views/livewire/showcase.blade.php
+++ resources/views/livewire/showcase.blade.php
@@ -4,31 +4,58 @@
 
 @section('content')
 @include('navbar.index')
+<head>
+    <link rel="stylesheet" type="text/css" href="{{ asset('Home/slick/slick.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('Home/slick/slick-theme.css') }}" />
 
-<main>
-    <div class="container">
-        <h1>SHOWCASE FOR OUR WORK !</h1>
-    </div>
-</main>
+    <!--===============================
+        FONT AWESOME FILE
+        ===============================-->
+    <link href="{{ asset('Home/fontawesome_css/all.css') }}" rel="stylesheet" type="text/css" />
 
-<div class="page1">
-    <div class="container text-center">
-        @foreach ($products as $product)
-            <div class="row service">
-                <div class="col-lg-6 col-md-7 mb-4 nopadding">
+    <!--===============================
+        AOS JS CDN
+        ===============================-->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head>
+<body>
+    <main>
+        <div class="container">
+            <h1>SHOWCASE FOR OUR WORK !</h1>
+        </div>
+    </main>
+
+    <div class="container project-items mt-4 mb-5">
+        <div class="row justify-content-center">
+            @foreach ($products as $product)
+                <div class="col-lg-6 col-md-7 mb-4">
                     <div class="card">
                         <img class="card-img-top" src="{{ asset('produkListJasa') }}/{{$product->image}}" alt="{{$product->name}}">
                         <div class="card-body">
                             <p class="card-text">{{$product->name}}</p>
                             <div class="wrap-price"><span class="product-price">{{$product->harga}}</span></div>
+                            <!-- Buttonnya percobaan doang  -->
                             <a href="#" class="btn add-to-cart" wire:click.prevent="store({{$product->id}},'{{$product->name}}',{{$product->harga}})">Add To Cart</a>
                         </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
-</div>
-</div>
-</div>
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <!--===============================
+        SLICK JS FILE -> For Image Carousel
+        ===============================-->
+    <script src="{{ asset('Home/slick/slick.min.js') }}"></script>
+    <!--===============================
+        AOS JS -> Animate On Scroll
+        ===============================-->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+</body>
+
+
 @endsection
